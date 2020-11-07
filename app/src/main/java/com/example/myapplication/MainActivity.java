231228a0324c9a1a678c48c51967c67566d02d4a
hamdi bel hadj youssef <45259633+bhyhamdi@users.noreply.github.com>
2020-11-08 00:39:30 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("tag", "message de debugage ");
+        Log.e("tag","message d'erreur " ) 
+        
     }
 }
